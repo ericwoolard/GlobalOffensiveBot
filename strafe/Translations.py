3f153374002c590690d26e4cb72b9d921a09dc80
Eric Woolard <premierproductions601@gmail.com>
2019-02-13 13:16:48 -0600

    Added translations for IEM Katowice CSGO Major

--- strafe/Translations.py
+++ strafe/Translations.py
@@ -6,7 +6,15 @@ class Translations(object):
             'Opening Matches': 'Opener',
             'Elimination Match': 'Elimination',
             'Winners\' Match': 'Winners',
-            'Decider Match': 'Decider'
+            'Decider Match': 'Decider',
+            'Round 2 Low': 'Rd. 2 Low',
+            'Round 2 High': 'Rd. 2 High',
+            'Round 3 Low': 'Rd. 3 Low',
+            'Round 3 Mid': 'Rd. 3 Mid',
+            'Round 3 High': 'Rd. 3 High',
+            'Round 4 Low': 'Rd. 4 Low',
+            'Round 4 Mid': 'Rd. 4 Mid',
+            'Round 4 High':, 'Rd. 4 High'
         }
 
     @property
